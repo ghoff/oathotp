f7bb3a021ec5fdcfda72f25039e7772268d4d36d
Geoffrey Hoff <geoff.hoff@gmail.com>
2009-05-23 15:00:02 -0400

    Webapp now allows new seed to be loaded and otp to be generated
    /id is currently in test mode and displays otp instead of validating it

--- oathotp.py
+++ oathotp.py
@@ -2,25 +2,42 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
+import hotpy
+import os
+import binascii
 
 class HotpData(db.Model):
-  id = db.IntegerProperty(required=True)
+  id = db.StringProperty(required=True)
   serialno = db.StringProperty(required=True)
-  salt = db.StringProperty(required=True)
-  secret = db.StringProperty(required=True)
+  iv = db.StringProperty(required=True)
+  seed = db.StringProperty(required=True)
   sequence = db.IntegerProperty(required=True)
-  #salt = db.ByteStringProperty(required=True)
-  #secret = db.ByteStringProperty(required=True)
+  otpdigits = db.IntegerProperty(required=True)
 
 class OtpPage(webapp.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/plain'
     if self.request.get('id'):
       querys = HotpData.all()
-      querys.filter('id =', int(self.request.get('id')))
-      if querys.count():
+      querys.filter('id =', self.request.get('id'))
+      if querys.count() == 1:
         for query in querys:
           self.response.out.write("%s %s\n" % (query.id, query.serialno))
+	  hconfig = dict()
+	  #fix this later, extract pin from full pin+otp
+	  hconfig['pincode'] = self.request.get('pin')
+	  hconfig['seed'] = query.seed
+	  hconfig['iv'] = query.iv
+	  hconfig['counter'] = query.sequence
+	  hconfig['digits'] = query.otpdigits
+	  # need to fix generator to work correctly with bad pin
+	  gen = hotpy.OTPGenerator(hconfig)
+          self.response.out.write(gen.getOTP())
+	  query.sequence = query.sequence + 1
+	  query.put()
+
+      elif querys.count() > 1:
+        self.response.out.write("Error: mutiple entries not yet supported\n")
       else:
         self.response.out.write("Error: invalid ID\n")
     else:
@@ -29,30 +46,31 @@ class OtpPage(webapp.RequestHandler):
 class AdminPage(webapp.RequestHandler):
   def get(self):
     self.response.headers['Content-Type'] = 'text/html'
+    self.response.out.write("<html><head></head><body>\n")
     user = users.get_current_user()
     if user and not users.is_current_user_admin():
-      self.response.out.write("<html><head></head><body>\n")
       self.response.out.write("Only admin users allowed\n")
       self.response.out.write("<br><a href=%s>Logout</a>" % users.create_logout_url(self.request.uri))
       self.response.out.write("</body></html>")
     elif user and users.is_current_user_admin():
-      self.response.out.write("<html><head></head><body>\n")
       self.response.out.write("add a new value\n")
       self.response.out.write("""
             <form action="/admin" method="post">
 	    <table>
 	      <tr><td>id</td><td><input name="id" size=20></td></tr>
               <tr><td>serialno</td><td><input name="serialno" size=20></td></tr>
-              <tr><td>salt</td><td><input name="salt" size=32></td></tr>
-              <tr><td>secret</td><td><input name="secret" size=32></td></tr>
-              <tr><td>sequence</td><td><input name="sequence" size=20></td></tr>
+              <tr><td>sequence</td><td><input name="sequence" size=16></td></tr>
+              <tr><td>pin</td><td><input name="pin" size=16></td></tr>
+              <tr><td>seed</td><td><input name="seed" size=64></td></tr>
+              <tr><td>otp digits</td><td><input name="otpdigits" size=64></td></tr>
 	    </table>
               <input type="submit" value="add entry"></div>
             </form>""")
       self.response.out.write("<br><a href=%s>Logout</a>" % users.create_logout_url(self.request.uri))
-      self.response.out.write("</body></html>")
     else:
       self.redirect(users.create_login_url(self.request.uri))
+      self.response.out.write("<br><a href=%s>Login</a>" % users.create_login_url(self.request.uri))
+    self.response.out.write("</body></html>")
 
   def post(self):
     user = users.get_current_user()
@@ -60,18 +78,33 @@ class AdminPage(webapp.RequestHandler):
       self.response.headers['Content-Type'] = 'text/plain'
       id=self.request.get('id')
       serialno=self.request.get('serialno')
-      salt=self.request.get('salt')
-      secret=self.request.get('secret')
       sequence=self.request.get('sequence')
+      pin=self.request.get('pin')
+      seed=self.request.get('seed')
+      otpdigits=self.request.get('otpdigits')
+
+      iv = binascii.b2a_hex(os.urandom(16))
+      hconfig = {}
+      hconfig['pincode'] = pin
+      hconfig['seed'] = seed
+      hconfig['iv'] = iv
+      encseed = hotpy.OTPGenerator(hconfig)
+      eseed = encseed.cryptSeed()
+
       self.response.out.write("id = %s\n" % id)
       self.response.out.write("serialno = %s\n" % serialno)
-      self.response.out.write("salt = %s\n" % salt)
-      self.response.out.write("secret = %s\n" % secret)
-      hdata = HotpData(id=int(id),
+      self.response.out.write("sequence = %s\n" % sequence)
+      self.response.out.write("pin = %s\n" % pin)
+      self.response.out.write("seed = %s\n" % seed)
+      self.response.out.write("iv = %s\n" % iv)
+      self.response.out.write("eseed = %s\n" % eseed)
+      self.response.out.write("otp digits = %s\n" % otpdigits)
+      hdata = HotpData(id=id,
         serialno=serialno,
-        salt=salt,
-        secret=secret,
-        sequence=int(sequence))
+        iv=iv,
+        seed=eseed,
+        sequence=int(sequence),
+	otpdigits=int(otpdigits))
       hdata.put()
     else:
       self.response.out.write("Access Denied.")
@@ -92,7 +125,7 @@ application = webapp.WSGIApplication(
                                      [('/otp', OtpPage),
                                      ('/admin', AdminPage),
                                      ('/', MainPage)],
-                                     debug=True)
+                                     debug=False)
 
 def main():
   run_wsgi_app(application)
